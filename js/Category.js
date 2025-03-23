d2a4b27356b161510a44721221beb6c4ab521db9
Saul <exmosaul@gmail.com>
2025-03-23 19:14:23 +0000

    Añadidas las series

--- js/Category.js
+++ js/Category.js
@@ -1,4 +1,16 @@
-obtenerFilms().then(films => {
-    LoadFilm_categories(films);
-    prepararRotacion()
+document.addEventListener("DOMContentLoaded", function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const categoryType = urlParams.get('name');
+    if (categoryType == "pelis") {
+        obtenerFilms().then(films => {
+            LoadFilm_categories(films);
+            prepararRotacion()
+        });
+    } else if (categoryType == "series") {
+        obtenerSeries().then(series => {
+            LoadFilm_categories(series);
+            prepararRotacion()
+        })
+    }
+
 });
--- js/Film.js
+++ js/Film.js
@@ -2,33 +2,39 @@ const urlParams = new URLSearchParams(window.location.search);
 const imageName = urlParams.get('name');
 
 obtenerFilms().then(films => {
-    let selectedFilm;
-    films.forEach((film, index) => {
-        if (film.Title === imageName) {
-            selectedFilm = film;
-        }
-    });
-    Promise.all([
-        loadTemplate("../templates/film.html", 'film'),
-        loadTemplate_class("../templates/category-right-arrow.html", 'c-right-arrow'),
-        loadTemplate_class("../templates/triangular-button-right.html", 't-button-right'),
-        loadTemplate_class("../templates/personal-review-read.html", 'read'),
-        loadTemplate_class("../templates/personal-review-write.html", 'write'),
-        loadTemplate_class("../templates/image-and-text-under.html", 'image-title')
-    ]).then(() => {
-        document.querySelector('#film_container div img').setAttribute('src', selectedFilm.CoverUrl);
-        document.getElementById('film_container').lastElementChild.innerHTML = selectedFilm.Description;
-        console.log(document.getElementById('film_title').children[0]);
-        document.getElementById('film_title').children[0].innerHTML = selectedFilm.Title;
-        for (let i = 0; i < selectedFilm.Valoration; i++) {
-            let starImg = document.createElement('img');
-            starImg.setAttribute('src', "../img/star.png");
-            document.getElementById('film_title').appendChild(starImg);
-        }
-        let articleHTML = "";
-        getReviews(selectedFilm).then(reviews => {
-            reviews.forEach(review => {
-                articleHTML += `
+    obtenerSeries().then(series => {
+        let selectedFilm;
+        films.forEach((film, index) => {
+            if (film.Title === imageName) {
+                selectedFilm = film;
+            }
+        });
+        series.forEach((serie, index) => {
+            if (serie.Title === imageName) {
+                selectedFilm = serie;
+            }
+        });
+        Promise.all([
+            loadTemplate("../templates/film.html", 'film'),
+            loadTemplate_class("../templates/category-right-arrow.html", 'c-right-arrow'),
+            loadTemplate_class("../templates/triangular-button-right.html", 't-button-right'),
+            loadTemplate_class("../templates/personal-review-read.html", 'read'),
+            loadTemplate_class("../templates/personal-review-write.html", 'write'),
+            loadTemplate_class("../templates/image-and-text-under.html", 'image-title')
+        ]).then(() => {
+            document.querySelector('#film_container div img').setAttribute('src', selectedFilm.CoverUrl);
+            document.getElementById('film_container').lastElementChild.innerHTML = selectedFilm.Description;
+            console.log(document.getElementById('film_title').children[0]);
+            document.getElementById('film_title').children[0].innerHTML = selectedFilm.Title;
+            for (let i = 0; i < selectedFilm.Valoration; i++) {
+                let starImg = document.createElement('img');
+                starImg.setAttribute('src', "../img/star.png");
+                document.getElementById('film_title').appendChild(starImg);
+            }
+            let articleHTML = "";
+            getReviews(selectedFilm).then(reviews => {
+                reviews.forEach(review => {
+                    articleHTML += `
                     <article>
                         <section class="personal-review">
                             <div>
@@ -40,20 +46,19 @@ obtenerFilms().then(films => {
                         </section>
                     </article>
                 `;
+                })
+                console.log(articleHTML);
+                document.getElementById("review-container").innerHTML = articleHTML;
             })
-            console.log(articleHTML);
-            document.getElementById("review-container").innerHTML = articleHTML;
-        })
-        document.getElementById('oval-button').addEventListener('click', async function(e) {
-            e.preventDefault();
-            getUser(EMAIL, PASSWORD).then(user => {
-                pushReview(user, selectedFilm);
-            })
+            document.getElementById('oval-button').addEventListener('click', async function(e) {
+                e.preventDefault();  // Evitar que el formulario se envíe de forma tradicional
+                getUser(EMAIL, PASSWORD).then(user => {
+                    pushReview(user, selectedFilm);
+                })
+
+            });
+            prepararRotacion()
 
         });
-        prepararRotacion()
-
-    });
-
-
+    })
 });
