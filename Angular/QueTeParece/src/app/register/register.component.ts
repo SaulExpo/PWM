ece8bf57ad79c03a5ce22a4410db6bc0e0257600
edulanarf <edulanarf@gmail.com>
2025-04-02 23:43:58 +0100

    Añadido register

--- Angular/QueTeParece/src/app/register/register.component.ts
+++ Angular/QueTeParece/src/app/register/register.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
-    imports: [
-        RouterLink
-    ],
+  imports: [
+    RouterLink,
+    FormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  name: string = '';
+  surname: string = '';
+  email: string = '';
+  password: string = '';
 
+  register(): void {
+    console.log('Nombre:', this.name);
+    console.log('Apellido:', this.surname);
+    console.log('Email:', this.email);
+    console.log('Contraseña:', this.password);
+  }
 }
