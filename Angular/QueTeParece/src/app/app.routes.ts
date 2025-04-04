9604854a2bc77ede1439083165ee73aa5f27884e
edulanarf <edulanarf@gmail.com>
2025-04-04 18:18:37 +0100

    creada las paginas de editar perfil y perfil

--- Angular/QueTeParece/src/app/app.routes.ts
+++ Angular/QueTeParece/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import {Routes} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
-import {ProfileComponent} from "./profile/profile.component";
 import {RegisterComponent} from "./register/register.component";
 import {HomeComponent} from "./home/home.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {EditProfileComponent} from "./edit-profile/edit-profile.component";
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
     { path: 'profile', component: ProfileComponent },
+    { path: 'editProfile', component: EditProfileComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent }
 ];
