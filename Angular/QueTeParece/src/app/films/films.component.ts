dda58a7556e244246deb0c34a50b2c2c9db89b83
saul <exmosaul@gmail.com>
2025-04-22 17:56:32 +0100

    Peliculas actualizadas

--- Angular/QueTeParece/src/app/films/films.component.ts
+++ Angular/QueTeParece/src/app/films/films.component.ts
@@ -2,33 +2,52 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {onAuthStateChanged, User} from "firebase/auth";
 import {auth, db} from "../services/firebase-config";
-import {doc, getDoc, setDoc} from "@angular/fire/firestore";
+import { CommonModule } from '@angular/common';  // Importa CommonModule
+import {collection, doc, getDoc, getDocs, setDoc} from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-films',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './films.component.html',
   styleUrl: './films.component.css'
 })
 
 export class FilmsComponent implements OnInit {
-  @ViewChild('userNameDiv', { static: false }) userNameDiv!: ElementRef;
+    films: { Category: string; Title: string; CoverUrl: string}[] = [];
 
-  ngOnInit() {
-    this.initializeOnAuthStateChanged();
-  }
+    ngOnInit() {
+        this.initializeOnAuthStateChanged();
+    }
 
-  private initializeOnAuthStateChanged() {
-    onAuthStateChanged(auth, async(user: User|null)=>{
-        const docRef = doc(db, `films/ph9z20bu0a5anne2j7je23hq`);
-        const document = await getDoc(docRef);
-        const userData = document.data();  // Obtener los datos del documento
-        let nameDb = userData?.['Category'];
-        let surnameDb = userData?.['Title'];
+    private initializeOnAuthStateChanged() {
+        onAuthStateChanged(auth, async (user: User | null) => {
+            if (user) {
+                try {
+                    // Obtén la colección 'films' de Firestore
+                    const docRef = collection(db, 'films');
+                    const collections = await getDocs(docRef);
 
-        if (this.userNameDiv && nameDb) {
-          this.userNameDiv.nativeElement.textContent = nameDb +"  "+ surnameDb;
-        }
-    });
-  }
+                    // Crear un array para almacenar los datos de todos los documentos
+                    this.films = [];
+
+                    // Itera sobre los documentos de la colección
+                    collections.forEach((document) => {
+                        const documentData = document.data();  // Obtener los datos del documento
+
+                        // Obtener las propiedades 'Category' y 'Title' del documento
+                        let categoryDb = documentData?.['Category'];
+                        let titleDb = documentData?.['Title'];
+                        let coverDb = documentData?.['CoverUrl'];
+
+                        // Si los datos existen, agrégalo al array 'films'
+                        if (categoryDb && titleDb && coverDb) {
+                            this.films.push({ Category: categoryDb, Title: titleDb , CoverUrl: coverDb});
+                        }
+                    });
+                } catch (error) {
+                    console.error('Error al obtener los documentos: ', error);
+                }
+            }
+        });
+    }
 }
