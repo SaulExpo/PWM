40bcb14c8e46b8ec3dbd47ae5ec3551ffabe14e1
Eduardo Arbelo Rua-Figueroa <89623126+edulanarf@users.noreply.github.com>
2025-04-25 18:14:24 +0100

    rutas añadidas hacia film info y carga de la pelicula

--- Angular/QueTeParece/src/app/film-info/film-info.component.ts
+++ Angular/QueTeParece/src/app/film-info/film-info.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {doc, getDoc} from "@angular/fire/firestore";
+import {db} from "../services/firebase-config";
+import {ActivatedRoute} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-film-info',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './film-info.component.html',
   styleUrl: './film-info.component.css'
 })
-export class FilmInfoComponent {
+export class FilmInfoComponent implements OnInit {
+  filmId: string | null = null;
+  filmData: any = null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  async ngOnInit() {
+    this.route.queryParams.subscribe(async params => {
+      this.filmId = params['id'];
+      if (this.filmId) {
+        const filmRef = doc(db, 'films', this.filmId);
+        const filmSnap = await getDoc(filmRef);
+        if (filmSnap.exists()) {
+          this.filmData = filmSnap.data();
+          console.log(this.filmData);
+        } else {
+          console.error('Película no encontrada.');
+        }
+      }
+    });
+  }
 }
