4cd8c2d49ccabf4a4c92f36b4fb1022b6b2afb31
edulanarf <edulanarf@gmail.com>
2025-04-26 13:31:39 +0100

    añadidas funcionalidades a film-info

--- Angular/QueTeParece/src/app/film-info/film-info.component.ts
+++ Angular/QueTeParece/src/app/film-info/film-info.component.ts
@@ -2,11 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {doc, getDoc} from "@angular/fire/firestore";
 import {db} from "../services/firebase-config";
 import {ActivatedRoute} from "@angular/router";
-import {CommonModule} from "@angular/common";
+import {CommonModule, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-film-info',
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './film-info.component.html',
   styleUrl: './film-info.component.css'
 })
@@ -14,16 +14,22 @@ export class FilmInfoComponent implements OnInit {
   filmId: string | null = null;
   filmData: any = null;
 
-  constructor(private route: ActivatedRoute) {}
+  stars: number[] = [];
 
+  constructor(private route: ActivatedRoute) {}
   async ngOnInit() {
     this.route.queryParams.subscribe(async params => {
       this.filmId = params['id'];
       if (this.filmId) {
         const filmRef = doc(db, 'films', this.filmId);
         const filmSnap = await getDoc(filmRef);
+
         if (filmSnap.exists()) {
           this.filmData = filmSnap.data();
+
+          this.stars = Array(this.filmData.Valoration).fill(1);
+          console.log(this.stars)
+
           console.log(this.filmData);
         } else {
           console.error('Película no encontrada.');
@@ -32,3 +38,4 @@ export class FilmInfoComponent implements OnInit {
     });
   }
 }
+
