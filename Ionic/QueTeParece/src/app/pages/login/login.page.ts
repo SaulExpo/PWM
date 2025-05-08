a6e9e291b30b6af9d13fb31dcd15a72992e4bd3b
edulanarf <edulanarf@gmail.com>
2025-05-08 15:53:23 +0100

    firebase para el login terminado

--- Ionic/QueTeParece/src/app/pages/login/login.page.ts
+++ Ionic/QueTeParece/src/app/pages/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {IonContent, IonHeader, IonTitle, IonToolbar} from "@ionic/angular/standalone";
 import {FormsModule} from "@angular/forms";
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import {auth} from "../../services/firebase-config";
 
 @Component({
   standalone: true,
@@ -16,10 +18,23 @@ import {FormsModule} from "@angular/forms";
   ]
 })
 export class LoginPage implements OnInit {
+  email= "";
+  password= "";
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  login() {
+    signInWithEmailAndPassword(auth, this.email, this.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log('Usuario registrado exitosamente:', user);
+      })
+      .catch((error) => {
+        console.error("❌ Error en el login:", error.message);
+      });
+  }
 }
