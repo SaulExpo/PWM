ca9f5c2a0b3d8db57956f247ce71983035285053
edulanarf <edulanarf@gmail.com>
2025-05-09 11:28:54 +0100

    Pagina de todas las peliculas

--- Ionic/QueTeParece/src/app/pages/all-films/all-films.page.ts
+++ Ionic/QueTeParece/src/app/pages/all-films/all-films.page.ts
@@ -1,24 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, QueryList, Renderer2, ViewChildren} from '@angular/core';
 import {IonicModule} from "@ionic/angular";
 import {IonContent} from "@ionic/angular/standalone";
+import {ActivatedRoute, RouterLink} from "@angular/router";
+import {NgForOf} from "@angular/common";
+import {collection, getDocs} from "@angular/fire/firestore";
+import {db} from "../../services/firebase-config";
 
 @Component({
   selector: 'app-all-films',
   templateUrl: './all-films.page.html',
   styleUrls: ['./all-films.page.scss'],
   imports: [
-    IonContent
+    IonContent,
+    RouterLink,
+    NgForOf
   ],
   standalone:true
 })
 export class AllFilmsPage implements OnInit {
-  LiveFilms=[];
-  AnimationFilms= [];
+  @ViewChildren('buttonRef') buttonsRef!: QueryList<any>;
+  constructor(private renderer: Renderer2, private route: ActivatedRoute) {}
 
-
-  constructor() { }
+  categoryType: string | null = null;
+  films: { Category: string; Title: string; CoverUrl: string, type: string, Id:string}[] = [];
+  AnimationFilms:{ Category: string; Title: string; CoverUrl: string, type: string, Id:string}[] = [];
+  LiveFilms: { Category: string; Title: string; CoverUrl: string, type: string, Id:string}[] = [];
 
   ngOnInit() {
+    this.initializeOnAuthStateChanged();
+    this.route.queryParams.subscribe(params => {
+      this.categoryType = params['name'];  // Obtén el parámetro 'name' de la URL
+    });
+  }
+
+  ngAfterViewInit(): void {
+    // Ahora que la vista está completamente inicializada, podemos trabajar con @ViewChildren
+  }
+
+  prepararRotacion() {
+    this.buttonsRef.forEach((button: any, index: number) => {
+      this.renderer.listen(button.nativeElement, 'click', () => {
+        const container = button.nativeElement.parentElement.parentElement.firstElementChild;
+        const firstImage = container.firstElementChild;
+        container.appendChild(firstImage);
+      });
+    });
+  }
+
+
+  private async initializeOnAuthStateChanged() {
+    try {
+      // Obtén la colección 'films' de Firestore
+      const docRef = collection(db, 'films');
+      const collections = await getDocs(docRef);
+
+      // Crear un array para almacenar los datos de todos los documentos
+      this.films = [];
+
+      // Itera sobre los documentos de la colección
+      collections.forEach((document) => {
+        const documentData = document.data();  // Obtener los datos del documento
+
+        // Obtener las propiedades 'Category' y 'Title' del documento
+        let categoryDb = documentData?.['Category'];
+        let titleDb = documentData?.['Title'];
+        let coverDb = documentData?.['CoverUrl'];
+        let typeDb = documentData?.['type'];
+        let idDb = documentData?.['id'];
+        let filmIdDb = document.id;
+
+        // Si los datos existen, agrégalo al array 'films'
+        if (categoryDb && titleDb && coverDb) {
+          this.films.push({ Category: categoryDb, Title: titleDb , CoverUrl: coverDb, type: typeDb, Id:filmIdDb});
+        }
+      });
+      if (this.categoryType !== undefined) {
+        this.films = this.films.filter(film => film.type === this.categoryType)
+      }
+      this.AnimationFilms = this.films.filter(film => film.Category === "Animation");
+      this.LiveFilms = this.films.filter(film => film.Category === "Live-Action");
+
+      setTimeout(() => {
+        this.prepararRotacion();
+      });
+
+
+    } catch (error) {
+      console.error('Error al obtener los documentos: ', error);
+    }
   }
 
 }
--- Ionic/QueTeParece/src/app/services/firebase-config.ts
+++ Ionic/QueTeParece/src/app/services/firebase-config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import {getFirestore} from "@angular/fire/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDVoUOYXHjElTc0hHnin2y69EcPoQVNcJI",
@@ -13,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app, "(default)");
 
-export {auth};
+export {auth, db};
